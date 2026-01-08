57816296fd218cf6dcdfacdf4137fa34c1bd826e
suganthan08 <sk2606251@gmail.com>
2026-01-08 10:21:14 +0530

    add allu report

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // Use Playwright HTML reporter and Allure reporter to produce allure-results
+  reporter: [
+    ['html'],
+    ['allure-playwright', { outputFolder: 'allure-results' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
